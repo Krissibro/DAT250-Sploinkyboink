978a44460a0a21e69a784f72071690c4747e0311
h591305 <591305@stud.hvl.no>
2024-11-18 13:13:36 +0100

    Fixed the backend in regard to JWT.

--- backend/src/main/kotlin/com/example/sploinkyboink/security/SecurityConfig.kt
+++ backend/src/main/kotlin/com/example/sploinkyboink/security/SecurityConfig.kt
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
@@ -19,13 +23,14 @@ class SecurityConfig (
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .csrf { it.disable() }
+            .cors{}
+            .csrf {it.disable()}
             .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers("/sploinkyboinkend/login","/sploinkyboinkend/register", "/sploinkyboinkend/logout").permitAll()
+                    .requestMatchers("/sploinkyboinkend/login","/sploinkyboinkend/register", "/sploinkyboinkend/logout", "/sploinkyboinkend/polls/active" ,"/sploinkyboinkend/polls").permitAll()
                     .anyRequest().authenticated()
-
             }
+            .sessionManagement{it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)}
             .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
@@ -36,6 +41,16 @@ class SecurityConfig (
         return JwtAuthenticationFilter(jwtService)
     }
 
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:5173") // Your frontend URL
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowCredentials = true  // Allow credentials (cookies)
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
 
 
 }
